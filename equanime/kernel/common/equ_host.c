c56ede311d45e16ad4adef33735072c38716bf47
Jorge Zapata <jorgeluis.zapata@gmail.com>
2009-10-12 23:38:35 +0000

    + Make the common host kernel module work + Add the dm6446 registers + More Cleanup

--- equanime/kernel/common/equ_host.c
+++ equanime/kernel/common/equ_host.c
@@ -14,113 +14,113 @@
 #define DRIVER_NAME "equanime_host"
 #define DRIVER_VERSION "0.1"
 
-struct equanime_host
-{
-	/* keep the uio_info here */
-	/* store every dma memory allocated */
-};
+
+static int size;
 
 /*============================================================================*
  *                                 Platform                                   *
  *============================================================================*/
-#if 0
-static int mp2530f_host_probe(struct platform_device *pdev)
+static int equ_probe(struct platform_device *pdev)
 {
 	struct uio_info *info;
-	struct resource *r;
-	int ret;
-	unsigned int size;
-	dma_addr_t map_dma;
+	unsigned long kmem_end;
+	unsigned long vmem;
 
-	printk(KERN_INFO "[Equanime] Host UIO Driver\n");
-
+	if (size <= 0)
+		return -EINVAL;
 	info = kzalloc(sizeof(struct uio_info), GFP_KERNEL);
 	if (!info)
 		return -ENOMEM;
-	/* TODO read the memory size */
-	/* check the boundings */
-	/*  block_virtp = (unsigned long) ioremap_nocache(block_start, length); */
-	info->mem[0].addr = r->start;
-	info->mem[0].size = r->end - r->start + 1;
-	info->mem[0].memtype = UIO_MEM_PHYS;
-	info->mem[0].internal_addr = ioremap(info->mem[0].addr, info->mem[0].size);
-
-	if (!info->mem[0].internal_addr)
+	/* get the kernel memory end */
+	kmem_end = virt_to_phys((void *)PAGE_OFFSET) +
+               (num_physpages << PAGE_SHIFT);
+	vmem = (unsigned long) ioremap_nocache(kmem_end, size);
+	if (!vmem)
 	{
-	                printk(KERN_ERR "Failed to remap mem 0\n");
-	                ret = -ENOMEM;
-	                goto out_get;
+		kfree(info);
+		return -EINVAL;
 	}
-	/* TODO Rotator interrupt */
 
+	info->mem[0].addr = kmem_end;
+	info->mem[0].size = size;
+	info->mem[0].memtype = UIO_MEM_PHYS;
+	info->mem[0].internal_addr = vmem;
 
 	info->version = DRIVER_VERSION;
 	info->name = DRIVER_NAME;
 
 	if (uio_register_device(&pdev->dev, info))
 	{
-		ret = -ENODEV;
-		goto out_irq;
+		iounmap(vmem);
+		kfree(info);
+		return -ENODEV;
 	}
-
 	platform_set_drvdata(pdev, info);
 
+	printk(KERN_INFO "[Equanime] Host UIO Driver @ %016lx (%d)\n", kmem_end, size);
+
 	return 0;
-out_unmap:
-	release_mem_region(r->start, r->end - r->start + 1);
-out_get:
-	kfree(info);
-	return ret;
 }
 
-static int mp2530f_host_remove(struct platform_device *pdev)
+static int equ_remove(struct platform_device *pdev)
 {
 	struct uio_info *info = platform_get_drvdata(pdev);
 
-	platform_set_drvdata(pdev, NULL);
+	iounmap(info->mem[0].internal_addr);
 	uio_unregister_device(info);
-
+	platform_set_drvdata(pdev, NULL);
 	kfree (info);
+
 	return 0;
 }
 
-static struct platform_driver mp2530f_host_driver = {
-	.driver		= {
+static struct platform_device equ_device = {
+	.name           = DRIVER_NAME,
+	.id             = -1,
+	.dev = {
+		.coherent_dma_mask      =  0xffffffffUL,
+	}
+};
+
+static struct platform_driver equ_driver = {
+	.driver		=
+	{
 		.name	= DRIVER_NAME,
 		.owner	= THIS_MODULE,
 	},
-	.probe		= mp2530f_host_probe,
-	.remove		= mp2530f_host_remove,
+	.probe		= equ_probe,
+	.remove		= equ_remove,
 };
-#endif
+
 /*============================================================================*
  *                                  Module                                    *
  *============================================================================*/
-static int __init equ_host_init_module(void)
+static int __init equ_init_module(void)
 {
-	unsigned long kmem_end;
 
-	/* get the kernel memory end */
-	kmem_end = virt_to_phys((void *)PAGE_OFFSET) +
-               (num_physpages << PAGE_SHIFT);
-	printk("KERNEL END = %016lx\n", kmem_end);
+	int ret;
 
-	return -EINVAL;
-	/* TODO Instead of using a platform driver just do the probe here */
-#if 0
-	return platform_driver_register(&equ_host_driver);
-#endif
+	ret = platform_device_register(&equ_device);
+	if (ret)
+		return ret;
+	ret = platform_driver_register(&equ_driver);
+	if (ret)
+	{
+		platform_device_unregister(&equ_device);
+	}
+	return ret;
 }
 
-static void __exit equ_host_exit_module(void)
+static void __exit equ_exit_module(void)
 {
-#if 0
-	platform_driver_unregister(&equ_host_driver);
-#endif
+	platform_driver_unregister(&equ_driver);
+	platform_device_del(&equ_device);
 }
 
-module_init(equ_host_init_module);
-module_exit(equ_host_exit_module);
+module_init(equ_init_module);
+module_exit(equ_exit_module);
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Jorge Luis Zapata");
+module_param(size, int, 0);
+MODULE_PARM_DESC(size, "Size of the pool in bytes");
---
+++ equanime/src/lib/drivers/dm6446/dm6446_regs.h
@@ -0,0 +1,179 @@
+#ifndef _DM6446_REGS_H
+#define _DM6446_REGS_H
+
+#define	VPBE_REG_BASE				IO_ADDRESS(0x01c72780)
+#define	VENC_REG_BASE				IO_ADDRESS(0x01c72400)
+#define	OSD_REG_BASE				IO_ADDRESS(0x01c72600)
+#define OSD_REG_SIZE				0x00000180
+
+struct dm6446_venc_regs
+{
+	uint32_t vmod;
+	uint32_t vidctl;
+	uint32_t vdpro;
+	uint32_t syncctl;
+	uint32_t hspls;
+	uint32_t vspls;
+	uint32_t hint;
+	uint32_t hstart;
+	uint32_t hvalid;
+	uint32_t vint;
+	uint32_t vstart;
+	uint32_t vvalid;
+	uint32_t hsdly;
+	uint32_t vsdly;
+	uint32_t yccctl;
+	uint32_t rgbctl;
+	uint32_t rgbclp;
+	uint32_t linectl;
+	uint32_t cullline;
+	uint32_t lcdout;
+	uint32_t brts;
+	uint32_t brtw;
+	uint32_t acctl;
+	uint32_t pwmp;
+	uint32_t pwmw;
+	uint32_t dclkctl;
+	uint32_t dclkptn0;
+	uint32_t dclkptn1;
+	uint32_t dclkptn2;
+	uint32_t dclkptn3;
+	uint32_t dclkptn0a;
+	uint32_t dclkptn1a;
+	uint32_t dclkptn2a;
+	uint32_t dclkptn3a;
+	uint32_t dclkhs;
+	uint32_t dclkhsa;
+	uint32_t dclkhr;
+	uint32_t dclkvs;
+	uint32_t dclkvr;
+	uint32_t capctl;
+	uint32_t capdo;
+	uint32_t capde;
+	uint32_t atr0;
+	uint32_t atr1;
+	uint32_t atr2;
+	uint32_t rsvd0;
+#if 0
+#define	VENC_EPSON_LCDCTL			(VENC_REG_BASE + 0xB4)
+#define	VENC_CASIO_LCDCTL			(VENC_REG_BASE + 0xB4)
+#define	VENC_UDISP_LCDCT			(VENC_REG_BASE + 0xB4)
+#define	VENC_STN_LCDCT				(VENC_REG_BASE + 0xB4)
+#endif
+
+	uint32_t vstat;
+	uint32_t ramadr;
+	uint32_t ramport;
+	uint32_t dactst;
+	uint32_t ycolvl;
+	uint32_t scprog;
+	uint32_t rsvd1;;
+	uint32_t rsvd2;;
+	uint32_t rsvd3;;
+	uint32_t cvbs;
+	uint32_t cmpnt;
+	uint32_t etmg0;
+	uint32_t etmg1;
+	uint32_t etmg2;
+	uint32_t etmg3;
+	uint32_t dacsel;
+	uint32_t rsvd4;;
+	uint32_t rsvd5;;
+	uint32_t argbx0;
+	uint32_t argbx1;
+	uint32_t argbx2;
+	uint32_t argbx3;
+	uint32_t argbx4;
+	uint32_t drgbx0;
+	uint32_t drgbx1;
+	uint32_t drgbx2;
+	uint32_t drgbx3;
+	uint32_t drgbx4;
+	uint32_t vstarta;
+	uint32_t osdclk0;
+	uint32_t osdclk1;
+	uint32_t hvldcl0;
+	uint32_t hvldcl1;
+	uint32_t osdhad;
+}
+
+struct dm6446_osd_regs
+{
+	uint32_t mode;
+	uint32_t vidwinmd;
+	uint32_t osdwin0md;
+	uint32_t osdwin1md;
+	uint32_t rectcur;
+	uint32_t rsvd0;
+	uint32_t vidwin0ofst;
+	uint32_t vidwin1ofst;
+	uint32_t osdwin0ofst;
+	uint32_t osdwin1ofst;
+	uint32_t vidwin0adr;
+	uint32_t rsvd1;
+	uint32_t vidwin1adr;
+	uint32_t rsvd2;
+	uint32_t osdwin0adr;
+	uint32_t osdwin1adr;
+	uint32_t basepx;
+	uint32_t basepy;
+	uint32_t vidwin0xp;
+	uint32_t vidwin0yp;
+	uint32_t vidwin0xl;
+	uint32_t vidwin0yl;
+	uint32_t vidwin1xp;
+	uint32_t vidwin1yp;
+	uint32_t vidwin1xl;
+	uint32_t vidwin1yl;
+	uint32_t osdwin0xp;
+	uint32_t osdwin0yp;
+	uint32_t osdwin0xl;
+	uint32_t osdwin0yl;
+	uint32_t osdwin1xp;
+	uint32_t osdwin1yp;
+	uint32_t osdwin1xl;
+	uint32_t osdwin1yl;
+	uint32_t curxp;
+	uint32_t curyp;
+	uint32_t curxl;
+	uint32_t curyl;
+	uint32_t rsvd3;
+	uint32_t rsvd4;
+	uint32_t w0bmp01;
+	uint32_t w0bmp23;
+	uint32_t w0bmp45;
+	uint32_t w0bmp67;
+	uint32_t w0bmp89;
+	uint32_t w0bmpab;
+	uint32_t w0bmpcd;
+	uint32_t w0bmpef;
+	uint32_t w1bmp0;
+	uint32_t w1bmp2;
+	uint32_t w1bmp4;
+	uint32_t w1bmp6;
+	uint32_t w1bmp8;
+	uint32_t w1bmpa;
+	uint32_t w1bmpc;
+	uint32_t w1bmpe;
+	uint32_t ti_tes;
+	uint32_t rsvd5;
+	uint32_t miscct;
+	uint32_t clutramyc;
+	uint32_t clutramc;
+	uint32_t transpva;
+	uint32_t rsvd6;
+	uint32_t ppvwin0ad;
+};
+
+struct dm6446_vpbe_regs
+{
+	uint32_t pid;
+	uint32_t pcr;
+};
+
+struct dm6446_vpss_regs
+{
+	uint32_t clkctl;
+}
+
+#endif
